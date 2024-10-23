bf7a905c4b84d425f49e43aee7dfd4afebbcc4b1
Jack Brosnan <ik7pdsoq@students.codeinstitute.net>
2024-10-23 15:15:09 +0000

    Updated video card creation script - Alignment and Margin adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -225,7 +225,7 @@ function displayVideos(e) {
     e.forEach(video => {
         const videoId = video.id.videoId;
         const videoCard = document.createElement('div');
-        videoCard.classList.add('col-12');
+        videoCard.classList.add('col-12', 'mb-3', 'd-flex', 'justify-content-center');
         //Create bootstrap card component and import video data
         videoCard.innerHTML = `
                     <div class = "card mb-1" style="max-width: 80%; width: 80%;">
